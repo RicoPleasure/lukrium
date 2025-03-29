6ab1e5a07360217e40ac39975f23215a2e26fc3c
Gonçalo Fernandes <149530330+gxnca@users.noreply.github.com>
2025-03-29 23:07:48 +0000

    feat: dashboard basic structure (#3)

---
+++ frontend/app/priceGenerator/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import PriceForm from "../components/priceForm";
+
+export default function PriceGenerator() {
+  return (
+    <PriceForm/>
+  );
+}
---
+++ frontend/app/review/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Review() {
+  return <div>Revisão</div>;
+}
